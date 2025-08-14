99fec766de1866691fa35bc94f4370b037434910
junseok816 <junseok816@gmail.com>
2025-08-14 14:43:49 +0900

    port변경

--- back-end/src/web.ts
+++ back-end/src/web.ts
@@ -127,8 +127,8 @@ console.log(
 );
 
 const server = app
-  .listen(3000, () => {
-    console.log(`Example app listening on port ${3000}`);
+  .listen(3001, () => {
+    console.log(`Example app listening on port ${3001}`);
   })
   .setTimeout(12000000);
 
